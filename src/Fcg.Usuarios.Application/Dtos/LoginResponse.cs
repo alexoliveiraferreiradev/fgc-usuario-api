f87e8b08fc79ed8ffd22b5b1b6f17c91058da361
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-07 18:17:59 -0300

    feat: add LoginResponse para retorno após o usuário fazer login

---
+++ src/Fcg.Usuarios.Application/Dtos/LoginResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Fcg.Usuarios.Application.Dtos
+{
+    public class LoginResponse
+    {
+        public string AcessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<ClaimResponse> Claims { get; set; }
+    }
+}
