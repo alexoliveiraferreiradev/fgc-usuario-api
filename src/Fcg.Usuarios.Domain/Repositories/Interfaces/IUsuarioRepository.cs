e4eb699c1e6986b92d94970e43b13b8e5b2eac96
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-07 18:06:34 -0300

    feat: add Interface IUsuarioRepository para implementações no banco

---
+++ src/Fcg.Usuarios.Domain/Repositories/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using Fcg.Usuarios.Domain.Entitites;
+
+namespace Fcg.Usuarios.Domain.Repositories.Interfaces
+{
+    public interface IUsuarioRepository 
+    {
+        Task Adicionar(Usuario usuario);
+        Task Atualizar(Usuario usuario);
+        Task<Usuario> ObterPorId(Guid id);
+        Task<IEnumerable<Usuario>> ObterTodos();
+        Task SaveChanges();
+        Task<Usuario?> ObterPorEmail(string email);
+        Task<bool> VerificaEmailCadastrado(string emailCadastrado);
+        Task<bool> VerificaMaisDeUmAdminCadastrado();
+        Task<bool> VerificaNomeCadastrado(string nomeCadastrado);
+        Task<bool> VerificaNomeCadastradoParaAlteracao(Guid usuarioId, string nomeCadastrado);
+    }
+}
