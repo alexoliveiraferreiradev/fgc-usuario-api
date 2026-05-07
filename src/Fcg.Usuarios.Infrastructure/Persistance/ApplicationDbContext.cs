147db4d711ab6cc1a7026086f4b0cc50c7f6ed4b
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-07 18:09:11 -0300

    feat: add ApplicationDbContext para integração com SQL Server

---
+++ src/Fcg.Usuarios.Infrastructure/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Fcg.Usuarios.Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fcg.Usuarios.Infrastructure.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
