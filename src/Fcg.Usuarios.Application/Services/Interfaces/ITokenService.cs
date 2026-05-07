f11b7906db9147b46683bfd6f18cb11ef1b28296
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-07 18:12:52 -0300

    feat: add ITokenService para implementar configurações do token

---
+++ src/Fcg.Usuarios.Application/Services/Interfaces/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using Fcg.Usuarios.Application.Dtos;
+using System.Security.Claims;
+
+namespace Fcg.Usuarios.Application.Services.Interfaces
+{
+    public interface ITokenService
+    {
+        string TokenGenerate(IEnumerable<Claim> claims);
+        Task<IEnumerable<Claim>> ObtemClaims(UsuarioResponse usuario);
+        Task<LoginResponse> RetornaJwt(UsuarioResponse usuario);
+    }
+}
