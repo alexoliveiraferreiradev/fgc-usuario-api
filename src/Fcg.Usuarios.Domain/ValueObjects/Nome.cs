fb6dd2113543d998ac325e81ac9b754638b160ca
Alex Oliveira <alexoliveiraferreiradev@gmail.com>
2026-05-07 17:55:32 -0300

    feat: add Nome para ValueObject

---
+++ src/Fcg.Usuarios.Domain/ValueObjects/Nome.cs
@@ -0,0 +1,27 @@
+﻿using Fcg.Usuarios.Domain.Common;
+using Fcg.Usuarios.Domain.Constants;
+
+namespace Fcg.Usuarios.Domain.ValueObjects
+{
+    public class Nome : ValueObject<Nome>
+    {
+        public string Valor { get; }
+        public Nome(string valor)
+        {
+            AssertionConcern.AssertArgumentRealValues(valor, MensagensDominio.NomeNaoReal);
+            AssertionConcern.AssertArgumentEmpty(valor, MensagensDominio.UsuarioNomeObrigatorio);
+            AssertionConcern.AssertArgumentLength(valor, 3, 50, MensagensDominio.UsuarioTamanhoNomeInvalido);
+            Valor = valor;
+        }
+
+        protected override bool EqualsCore(Nome other)
+        {
+            return Valor == other.Valor;
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            return Valor.GetHashCode();
+        }
+    }
+}
